bc04a83af9bc23f5ec646d1eeb3250f7be3bcb3d
Max Beizer <max.beizer@gmail.com>
2024-02-01 15:46:47 +0000

    Add a web validator but this is not safe to deploy

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Hello, World!"
+  return render_template('home.html')
+
+
+@app.route('/validate', methods=['POST'])
+def submit():
+  file = request.form['file']
+  result = subprocess.run(['rstcheck', '-'], input=file, text=True, capture_output=True)
+  if result.stderr:
+      print('Errors found:')
+      print(result.stderr)
+      response = result.stderr.split('<stdin>:')
+  else:
+      print('No errors found.')
+      response = 'No errors found.'
+  return render_template('response.html', response=response)
 
 if __name__ == "__main__":
   app.run(debug=True)
